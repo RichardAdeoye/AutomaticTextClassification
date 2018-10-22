0318973683a1c89ad88db50b5f12f6f9a9d5fa32
RichardAdeoye <adeoyerichard@yahoo.co.uk>
2018-10-22 11:07:01 +0100

    add freqeuncy list

--- AutomaticTextClassification/AutomaticTextClassification/AutomaticTextClassification/Program.cs
+++ AutomaticTextClassification/AutomaticTextClassification/AutomaticTextClassification/Program.cs
@@ -13,24 +13,47 @@ namespace AutomaticTextClassification
     {
         static void Main(string[] args)
         {
+            //var trainingCatergories = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory()));
+
             var textFilePath = Path.Combine(Directory.GetCurrentDirectory().Replace("\\AutomaticTextClassification\\bin\\Debug", ""), "Conservative27thMay2015.txt");
             //Create user input for files and separate training and test file variables
             var textFile = File.ReadAllText(textFilePath);
             textFile = textFile.Replace("\n", "");
             textFile = textFile.Replace("\r", "");
             var trainingTextWords = textFile.Split(' ', ',', '.').Distinct().ToList();//Unique words in training set
-           
-            List<string> trainingTextList = RemoveStopWords(trainingTextWords).ToList();
 
-            foreach (var word in textFile.Split(' ', ',', '.'))// total number of unique words throughout the training documents (Frequency)
+            var textWords = textFile.Split(' ', ',', '.').ToList();
+
+            List<string> trainingTextList = RemoveStopWords(textWords).ToList();
+
+            List<int> frequency = new List<int>();
+            List<string> words = new List<string>();
+
+            foreach (var word in trainingTextList)// total number of unique words throughout the training documents (Frequency)
             {
-                if (word != "")
-                {
-                    var trainingWordFrequency = textFile.Split(' ', ',', '.').Count(x => x == word);
-                    Console.WriteLine(word + ": " + trainingWordFrequency);
-                }
+                int trainingWordFrequency;
+                    if (word != "")
+                    {
+                        trainingWordFrequency = trainingTextList.Count(x => x == word);
+                        //Console.WriteLine(word + ": " + trainingWordFrequency);
+                    words.Add(word);
+                    frequency.Add(trainingWordFrequency);
+                    
+                    };
+                        
 
             }
+            var i = 0;
+            foreach(var x in words)
+            {
+                if (words.Contains(x))
+                {
+                    Console.WriteLine(x + ":" + frequency[i]);
+                    i++;
+                }
+                
+            }
+
             var csv = new StringBuilder();
             csv.AppendLine(trainingTextList.ToString());
             string csvPath =
